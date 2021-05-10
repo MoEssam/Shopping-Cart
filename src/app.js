b81b9e6c6f551668bfcfb74fad18ca8a6b9d0bee
Mohamed Essam <mohamedessam.moe@gmail.com>
2021-05-10 15:52:28 +0200

    fixed not found routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 var express = require("express");
-var router = express.Router();
 
 var app = express();
 app.use(express.json());
@@ -14,7 +13,7 @@ app.get("/", (req, res) => {
 app.use("/users", require("./user/index"));
 
 // Error handler - when none of the endpoints match
-router.use((req, res, next) => {
+app.use((req, res, next) => {
   res.status(404).json({
     error: {
       message: "404 not found",
